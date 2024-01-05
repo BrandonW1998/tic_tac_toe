295d9ffae7f3ba324cb42e08a3b0daa7d7dce60c
Brandon Westbrook <brandon.westbrook98@gmail.com>
2024-01-05 15:14:28 -0500

    Added turns, hasWinner(), updated README

--- DisplayGame.java
+++ DisplayGame.java
@@ -1,10 +1,16 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class DisplayGame extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private Container pane;	// Contains board
-	private JButton[][] gameBoard; // 2D Array of buttons
+	private JButton[][] gameBoard;	// 2D Array of buttons
+	private String currPlayer;	// Tracks current player
+	private int turnCount;	// Tracks turns
+	private boolean hasWinner;	// Flag set when winner found
 	private static final int boardRow = 3;	// Board size
 	private static final int boardCol = 3;	// Board size
 	
@@ -16,7 +22,7 @@ public class DisplayGame extends JFrame{
 		
 		// Initialize pane and layout
 		pane = getContentPane();
-		pane.setLayout(new GridLayout(boardRow, boardCol));
+		pane.setLayout(new GridLayout(boardRow + 1, boardCol));
 		
 		// Initialize frame
 		setTitle("Tic-Tac-Toe");
@@ -26,6 +32,9 @@ public class DisplayGame extends JFrame{
 		
 		// Initialize game variables
 		gameBoard = new JButton[boardRow][boardCol];
+		currPlayer = "X";
+		turnCount = 1;
+		hasWinner = false;
 		
 		// Create/Display board
 		initBoard();
@@ -42,9 +51,120 @@ public class DisplayGame extends JFrame{
 				button.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 125));
 				gameBoard[i][j] = button;
 				
+				button.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (((JButton) e.getSource()).getText().equals("")) {
+							button.setText(currPlayer);
+							hasWinner();
+							if (hasWinner) {
+								// TODO Call Reset Game
+							}
+							turnCount++;
+							playerToggle();
+						}
+						
+					}
+				});
+				
 				// Add button to pane
 				pane.add(button);
 			}
+		}
+	}
+	
+	// Change players
+	private void playerToggle() {
+		
+		// Swap current player from X > O, or vice versa
+		if (currPlayer.equals("X")) {
+			currPlayer = "O";
+		} else {
+			currPlayer = "X";
+		}
+	}
+	
+	// Scan board for winner or tie
+	// If winner/tie end game
+	// Else return to game
+	private void hasWinner() {
+		
+		// Left Column Win
+		if (	gameBoard[0][0].getText().contentEquals(currPlayer) &&
+				gameBoard[1][0].getText().contentEquals(currPlayer) &&
+				gameBoard[2][0].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Middle Column Win
+		else if(gameBoard[0][1].getText().contentEquals(currPlayer) &&
+				gameBoard[1][1].getText().contentEquals(currPlayer) &&
+				gameBoard[2][1].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Right Column Win
+		else if(gameBoard[0][2].getText().contentEquals(currPlayer) &&
+				gameBoard[1][2].getText().contentEquals(currPlayer) &&
+				gameBoard[2][2].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Top Row Win
+		else if(gameBoard[0][0].getText().contentEquals(currPlayer) &&
+				gameBoard[0][1].getText().contentEquals(currPlayer) &&
+				gameBoard[0][2].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Middle Row Win
+		else if(gameBoard[1][0].getText().contentEquals(currPlayer) &&
+				gameBoard[1][1].getText().contentEquals(currPlayer) &&
+				gameBoard[1][2].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Bottom Row Win
+		else if(gameBoard[2][0].getText().contentEquals(currPlayer) &&
+				gameBoard[2][1].getText().contentEquals(currPlayer) &&
+				gameBoard[2][2].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Diagonal Top-Left Win
+		else if(gameBoard[0][0].getText().contentEquals(currPlayer) &&
+				gameBoard[1][1].getText().contentEquals(currPlayer) &&
+				gameBoard[2][2].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Diagonal Top-Right Win
+		else if(gameBoard[0][2].getText().contentEquals(currPlayer) &&
+				gameBoard[1][1].getText().contentEquals(currPlayer) &&
+				gameBoard[2][0].getText().contentEquals(currPlayer))
+		{
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "Player " + currPlayer + " Wins!\n\nNew Game?");
+		}
+		
+		// Tie Game
+		else if (turnCount == (boardRow * boardCol)) {
+			hasWinner = true;
+			JOptionPane.showMessageDialog(this, "It's a tie game!\n\nNew Game?");
 		}
 	}
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Tic Tac Toe Game 
+
+Files: Game.java, DisplayGame.java
+
+"Game.java" is the driver file, which references DisplayGame to generate the GUI for gameplay
